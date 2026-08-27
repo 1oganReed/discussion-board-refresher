2cf17a45829f5b901bc688375b261b8521859417
1oganReed <reedchristopherlogan@gmail.com>
2026-08-27 09:27:24 -0700

    day 1 work; beginning HTML template

---
+++ students.js
@@ -0,0 +1,4 @@
+const discussionBoard = document.getElementById('discussion-board');
+
+discussionBoard.innerHTML = 
+'<article class="discussion-post" class="version-one" id="post=101"><header></header><h3 class="post-title"></h3><img class="article-image" src="" alt="Media Banner"><p class="post-message"></p><div class="post-categories"><span class="category"></span><span class="category"></span><span class="category"></span></div></article>'
