c585f96731166b86949dff8148c24003fcb567a3
1oganReed <reedchristopherlogan@gmail.com>
2026-09-01 09:28:34 -0700

    js

--- students.js
+++ students.js
@@ -63,7 +63,77 @@ article.classList.add('discussion-post','version-two');
 discussionBoard.appendChild(article);
 
 let backgroundImg = document.createElement('img');
-article.classList.add('article-image');
+backgroundImg.classList.add('article-image');
 article.appendChild(backgroundImg);
 
 let verTwoOverlay = document.createElement('div');
+verTwoOverlay.classList.add('version-two-overlay');
+article.appendChild(verTwoOverlay);
+
+let topRow = document.createElement('div');
+topRow.classList.add('post-top-row');
+verTwoOverlay.appendChild(topRow);
+
+let postCategories = document.createElement('div');
+postCategories.classList.add('post-categories');
+topRow.appendChild(postCategories);
+
+let category = document.createElement('span');
+category.classList.add('category');
+postCategories.appendChild(category);
+
+let postAuthor = document.createElement('div');
+postAuthor.classList.add('post-author');
+topRow.appendChild(postAuthor);
+
+let profileImg = document.createElement('img');
+profileImg.classList.add('profile-image');
+postAuthor.appendChild(profileImg);
+
+let authInfo = document.createElement('div');
+authInfo.classList.add('author-info');
+postAuthor.appendChild(authInfo);
+
+let authName = document.createElement('h2');
+authName.classList.add('author-name');
+authInfo.appendChild(authName);
+
+let authUsername = document.createElement('p');
+authUsername.classList.add('author-username');
+authInfo.appendChild(authUsername);
+
+let glassContent = document.createElement('div');
+glassContent.classList.add('post-glass-content');
+verTwoOverlay.appendChild(glassContent);
+
+let postTitle = document.createElement('h3');
+postTitle.classList.add('post-title');
+glassContent.appendChild(postTitle);
+
+let postMsg = document.createElement('p');
+postMsg.classList.add('post-message');
+glassContent.appendChild(postMsg);
+
+let bottomRow = document.createElement('div');
+bottomRow.classList.add('post-bottom-row');
+glassContent.appendChild(bottomRow);
+
+let postedDate = document.createElement('p');
+postedDate.classList.add('post-date');
+bottomRow.appendChild(postedDate);
+
+let postEngagement = document.createElement('div');
+postEngagement.classList.add('post-engagement');
+bottomRow.appendChild(postEngagement);
+
+let likes = document.createElement('p');
+likes.classList.add('likes');
+postEngagement.appendChild(likes);
+
+let views = document.createElement('p');
+views.classList.add('views');
+postEngagement.appendChild(views);
+
+let replies = document.createElement('p');
+replies.classList.add('replies');
+postEngagement.appendChild(replies);
